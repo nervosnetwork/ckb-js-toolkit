53644c5fe028045608fe5d6a2a1dc535e8729d24
Xuejie Xiao <xxuejie@gmail.com>
2020-03-06 01:31:53 +0000

    More data structures

--- src/validators/index.js
+++ src/validators/index.js
@@ -2,48 +2,59 @@
 // following the correct format, so we can submit them to CKB via RPC
 // directly
 
-function assertObjectWithKeys(object, ...expectedKeys) {
+function assertObject(object, debugPath = null) {
+  debugPath = debugPath || "Provided value"
   if (object === null ||
       typeof object !== "object" ||
       object.constructor !== Object) {
-    throw new Error("Provide value is not an object!");
+    throw new Error(`${debugPath} is not an object!`);
   }
-  const sortedProvidedKeys = Object.keys(object).sort();
-  const sortedExpectedKeys = expectedKeys.sort();
-  if (sortedProvidedKeys.length !== sortedExpectedKeys.length) {
+}
+
+function assertObjectWithKeys(object, expectedKeys, optionalKeys = []) {
+  assertObject(object);
+  const providedKeys = Object.keys(object).sort();
+  const requiredLength = expectedKeys.length;
+  const maximalLength = expectedKeys.length + optionalKeys.length;
+  if (providedKeys.length < requiredLength || providedKeys.length > maximalLength) {
     throw new Error("Specified object does not have correct keys!");
   }
-  for (let i = 0; i < sortedProvidedKeys.length; i++) {
-    if (sortedProvidedKeys[i] !== sortedExpectedKeys[i]) {
-      throw new Error("Specified object does not have correct keys!");
-    }
+  let optionalProvidedKeys = providedKeys.filter(key => !expectedKeys.includes(key));
+  if (providedKeys.length - optionalProvidedKeys.length !== requiredLength) {
+    throw new Error("Specified object does not have correct keys!");
+  }
+  if (optionalProvidedKeys.find(key => !optionalKeys.includes(key))) {
+    throw new Error("Specified object does not have correct keys!");
   }
 }
 
-function assertHexString(string, fieldName) {
+function assertHexString(string, debugPath = null) {
+  debugPath = debugPath || "Provided value";
   if (!/^0x([0-9a-fA-F][0-9a-fA-F])*$/.test(string)) {
-    throw new Error(`${fieldName} must be a hex string!`)
+    throw new Error(`${debugPath} must be a hex string!`)
   }
 }
 
-function assertHash(hash, fieldName) {
-  assertHexString(hash, fieldName);
+function assertHash(hash, debugPath = null) {
+  debugPath = debugPath || "Provided value";
+  assertHexString(hash, debugPath);
   if (hash.length != 66) {
-    throw new Error(`${fieldName} must be a hex string of 66 bytes long!`);
+    throw new Error(`${debugPath} must be a hex string of 66 bytes long!`);
   }
 }
 
-function assertInteger(i, fieldName) {
+function assertInteger(i, debugPath = null) {
+  debugPath = debugPath || "Provided value";
   if (i === "0x0") {
     return;
   }
   if (!/^0x[1-9a-fA-F][0-9a-fA-F]*$/.test(i)) {
-    throw new Error(`${fieldName} must be a hex integer!`)
+    throw new Error(`${debugPath} must be a hex integer!`)
   }
 }
 
 export function ValidateScript(script) {
-  assertObjectWithKeys(script, "code_hash", "hash_type", "args");
+  assertObjectWithKeys(script, ["code_hash", "hash_type", "args"], []);
   assertHash(script.code_hash, "code_hash");
   assertHexString(script.args, "args");
 
@@ -53,7 +64,73 @@ export function ValidateScript(script) {
 }
 
 export function ValidateOutPoint(outPoint) {
-  assertObjectWithKeys(outPoint, "tx_hash", "index");
+  assertObjectWithKeys(outPoint, ["tx_hash", "index"], []);
   assertHash(outPoint.tx_hash, "tx_hash");
   assertInteger(outPoint.index, "index");
+}
+
+export function ValidateCellInput(cellInput, nestedValidation = true) {
+  assertObjectWithKeys(cellInput, ["since", "previous_output"], []);
+  assertInteger(cellInput.since, "since");
+
+  if (nestedValidation) {
+    ValidateOutPoint(cellInput.previous_output);
+  }
+}
+
+export function ValidateCellOutput(cellOutput, nestedValidation = true) {
+  assertObjectWithKeys(cellOutput, ["capacity", "lock"], ["type"]);
+  assertInteger(cellOutput.capacity, "capacity");
+
+  if (nestedValidation) {
+    ValidateScript(cellOutput.lock);
+    if (cellOutput.type) {
+      ValidateScript(cellOutput.type);
+    }
+  }
+}
+
+export function ValidateCellDep(cellDep, nestedValidation = true) {
+  assertObjectWithKeys(cellDep, ["out_point", "dep_type"], []);
+  if (cellDep.dep_type !== "code" && cellDep.dep_type !== "dep_group") {
+    throw new Error("dep_type must be either code or dep_group!");
+  }
+
+  if (nestedValidation) {
+    ValidateOutPoint(cellDep.out_point);
+  }
+}
+
+function assertArray(array, validateFunction, nestedValidation) {
+  if (!Array.isArray(array)) {
+    throw new Error("Provided value is not an array!");
+  }
+  if (nestedValidation) {
+    for (const item of array) {
+      validateFunction(item);
+    }
+  }
+}
+
+function validateTransactionStructure(rawTransaction, nestedValidation) {
+  assertInteger(rawTransaction.version, "version");
+  assertArray(rawTransaction.cell_deps, ValidateCellDep, nestedValidation);
+  assertArray(rawTransaction.header_deps, assertHash, nestedValidation);
+  assertArray(rawTransaction.inputs, ValidateCellInput, nestedValidation);
+  assertArray(rawTransaction.outputs, ValidateCellOutput, nestedValidation);
+  assertArray(rawTransaction.outputs_data, assertHexString, nestedValidation);
+}
+
+export function ValidateRawTransaction(rawTransaction, nestedValidation = true) {
+  assertObjectWithKeys(rawTransaction, ["version", "cell_deps", "header_deps",
+                                        "inputs", "outputs", "outputs_data"], []);
+  validateTransactionStructure(rawTransaction, nestedValidation);
+}
+
+export function ValidateTransaction(transaction, nestedValidation = true) {
+  assertObjectWithKeys(transaction, ["version", "cell_deps", "header_deps",
+                                     "inputs", "outputs", "outputs_data",
+                                     "witnesses"], []);
+  validateTransactionStructure(transaction, nestedValidation);
+  assertArray(transaction.witnesses, assertHexString, nestedValidation);
 }
--- tests/validators.js
+++ tests/validators.js
@@ -82,3 +82,271 @@ test("outpoint with invalid hex number", t => {
     });
   });
 });
+
+test("correct cellinput", t => {
+  validators.ValidateCellInput({
+    since: "0x10",
+    previous_output: {
+      tx_hash: "0xa98c57135830e1b91345948df6c4b8870828199a786b26f09f7dec4bc27a73da",
+      index: "0x0"
+    }
+  });
+  t.pass();
+});
+
+test("cellinput with invalid since", t => {
+  t.throws(() => {
+    validators.ValidateCellInput({
+      since: "0x0001",
+      previous_output: {
+        tx_hash: "0xa98c57135830e1b91345948df",
+        index: "0x0"
+      }
+    });
+  });
+});
+
+test("cellinput with invalid outpoint", t => {
+  t.throws(() => {
+    validators.ValidateCellInput({
+      since: "0x0",
+      previous_output: {
+        tx_hash: "0xa98c57135830e1b91345948df",
+        index: "0x0"
+      }
+    });
+  });
+});
+
+test("cellinput with invalid outpoint but skip nested validation", t => {
+  validators.ValidateCellInput({
+    since: "0x0",
+    previous_output: {
+      tx_hash: "0xa98c57135830e1b91345948df",
+      index: "0x0"
+    }
+  }, false);
+  t.pass();
+});
+
+test("correct celloutput", t => {
+  validators.ValidateCellOutput({
+    capacity: "0x10",
+    lock: {
+      code_hash: "0xa98c57135830e1b91345948df6c4b8870828199a786b26f09f7dec4bc27a73da",
+      args: "0x1234",
+      hash_type: "data"
+    }
+  });
+  t.pass();
+});
+
+test("correct celloutput with type", t => {
+  validators.ValidateCellOutput({
+    capacity: "0x10",
+    lock: {
+      code_hash: "0xa98c57135830e1b91345948df6c4b8870828199a786b26f09f7dec4bc27a73da",
+      args: "0x1234",
+      hash_type: "data"
+    },
+    type: {
+      code_hash: "0xa98c57135830e1b91345948df6c4b8870828199a786b26f09f7dec4bc27a73da",
+      args: "0x",
+      hash_type: "type"
+    }
+  });
+  t.pass();
+});
+
+test("celloutput with invalid capacity", t => {
+  t.throws(() => {
+    validators.ValidateCellOutput({
+      capacity: "0xggg",
+      lock: {
+        code_hash: "0xa98c57135830e1b91345948df6c4b8870828199a786b26f09f7dec4bc27a73da",
+        args: "0x1234",
+        hash_type: "data"
+      }
+    });
+  });
+});
+
+test("celloutput with invalid lock", t => {
+  t.throws(() => {
+    validators.ValidateCellOutput({
+      capacity: "0x10",
+      lock: {
+        invalid: "lock"
+      }
+    });
+  });
+});
+
+test("celloutput with invalid lock but skips validation", t => {
+  validators.ValidateCellOutput({
+    capacity: "0x10",
+    lock: {
+      invalid: "lock"
+    }
+  }, false);
+  t.pass();
+});
+
+test("correct celldep", t => {
+  validators.ValidateCellDep({
+    dep_type: "code",
+    out_point: {
+      tx_hash: "0xa98c57135830e1b91345948df6c4b8870828199a786b26f09f7dec4bc27a73da",
+      index: "0x0"
+    }
+  });
+  t.pass();
+});
+
+test("celldep with invalid dep type", t => {
+  t.throws(() => {
+    validators.ValidateCellDep({
+      dep_type: "data",
+      out_point: {
+        tx_hash: "0xa98c57135830e1b91345948df6c4b8870828199a786b26f09f7dec4bc27a73da",
+        index: "0x0"
+      }
+    });
+  });
+});
+
+test("celldep with invalid out point", t => {
+  t.throws(() => {
+    validators.ValidateCellDep({
+      dep_type: "dep_group",
+      out_point: "invalid out point"
+    });
+  });
+});
+
+test("celldep with invalid out point but skips validation", t => {
+  validators.ValidateCellDep({
+    dep_type: "dep_group",
+    out_point: "invalid out point"
+  }, false);
+  t.pass();
+});
+
+test("correct raw transaction", t => {
+  validators.ValidateRawTransaction({
+    version: "0x0",
+    cell_deps: [
+      {
+        dep_type: "code",
+        out_point: {
+          tx_hash: "0xa98c57135830e1b91345948df6c4b8870828199a786b26f09f7dec4bc27a7300",
+          index: "0x0"
+        }
+      }
+    ],
+    header_deps: [
+      "0xb39d53656421d1532dd995a0924441ca8f43052bc2b7740a0e814a488a8214d6"
+    ],
+    inputs: [
+      {
+        since: "0x10",
+        previous_output: {
+          tx_hash: "0xa98c57135830e1b91345948df6c4b8870828199a786b26f09f7dec4bc27a7301",
+          index: "0x2"
+        }
+      }
+    ],
+    outputs: [
+      {
+        capacity: "0x1234",
+        lock: {
+          code_hash: "0xa98c57135830e1b91345948df6c4b8870828199a786b26f09f7dec4bc27a7302",
+          args: "0x1234",
+          hash_type: "data"
+        }
+      }
+    ],
+    outputs_data: [
+      "0xabcdef"
+    ]
+  });
+  t.pass();
+});
+
+test("invalid raw transaction", t => {
+  t.throws(() => {
+    validators.ValidateRawTransaction({
+      version: "0x0",
+      cell_deps: "invalid",
+      header_deps: [
+        "0xb39d53656421d1532dd995a0924441ca8f43052bc2b7740a0e814a488a8214d6"
+      ],
+      inputs: [
+        {
+          since: "0x10",
+          previous_output: {
+            tx_hash: "0xa98c57135830e1b91345948df6c4b8870828199a786b26f09f7dec4bc27a7301",
+            index: "0x2"
+          }
+        }
+      ],
+      outputs: [
+        {
+          capacity: "0x1234",
+          lock: {
+            code_hash: "0xa98c57135830e1b91345948df6c4b8870828199a786b26f09f7dec4bc27a7302",
+            args: "0x1234",
+            hash_type: "data"
+          }
+        }
+      ],
+      outputs_data: [
+        "0xabcdef"
+      ]
+    });
+  });
+});
+
+test("correct transaction", t => {
+  validators.ValidateTransaction({
+    version: "0x0",
+    cell_deps: [
+      {
+        dep_type: "code",
+        out_point: {
+          tx_hash: "0xa98c57135830e1b91345948df6c4b8870828199a786b26f09f7dec4bc27a7300",
+          index: "0x0"
+        }
+      }
+    ],
+    header_deps: [
+      "0xb39d53656421d1532dd995a0924441ca8f43052bc2b7740a0e814a488a8214d6"
+    ],
+    inputs: [
+      {
+        since: "0x10",
+        previous_output: {
+          tx_hash: "0xa98c57135830e1b91345948df6c4b8870828199a786b26f09f7dec4bc27a7301",
+          index: "0x2"
+        }
+      }
+    ],
+    outputs: [
+      {
+        capacity: "0x1234",
+        lock: {
+          code_hash: "0xa98c57135830e1b91345948df6c4b8870828199a786b26f09f7dec4bc27a7302",
+          args: "0x1234",
+          hash_type: "data"
+        }
+      }
+    ],
+    outputs_data: [
+      "0xabcdef"
+    ],
+    witnesses: [
+      "0x1111"
+    ]
+  });
+  t.pass();
+});
